094e85ccdd79d79bd9103aa35345d21c06d9dd85
NafizurRahman <117521573+NafizurRahman@users.noreply.github.com>
2022-12-25 23:58:19 +0600

    Add files via upload

--- test.php
+++ test.php
@@ -2,6 +2,6 @@
  $con = mysqli_connect('localhost','root','','lab-project final');
 if($con)
 echo 'connected';
-$sql = "INSERT INTO `book`( `typeNameX ` ,	 `typeEmailX  `	,`typeDateX` 	,`typeAddressX` 	,`typeSeatX `	) VALUES ('test', 'test','test', 'test', 'test')";
+$sql = "INSERT INTO `book`( `typeNameX` ,	 `typeEmailX`	,`typeDateX` 	,`typeAddressX` 	,`typeSeatX`	) VALUES ('test', 'test','test', 'test', 'test')";
 
 $rs= mysqli_query($con, $sql);
